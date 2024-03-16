18c8d974ff6199667ea05971c4e809bfb507f4a8
Béres Ákos Iván <h043580@stud.u-szeged.hu>
2024-03-16 14:32:59 +0100

    basic navbar

---
+++ pages/elements/navbar.php
@@ -0,0 +1,11 @@
+<nav id="navigacio">
+    <ul>
+        <li><a href="/Webterv-projekt-2024/index.php">Kezdőlap</a></li>
+        <li><a href="/Webterv-projekt-2024/pages/register.php">Regisztráció</a></li>
+        <li><a href="/Webterv-projekt-2024/pages/profile.php">Profil</a></li>
+        <li><a href="/Webterv-projekt-2024/pages/products.php">Termékek</a></li>
+        <li><a href="/Webterv-projekt-2024/pages/login.php">Bejelentkezés</a></li>
+        <li><a href="/Webterv-projekt-2024/pages/cart.php">Kosár</a></li>
+        <li><a href="/Webterv-projekt-2024/pages/admin.php">Admin</a></li>
+    </ul>
+</nav>
