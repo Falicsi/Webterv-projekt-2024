b98398ecd7c25cc799b8e624ff8c860a7b01a07e
Béres Ákos Iván <h043580@stud.u-szeged.hu>
2024-03-17 18:39:40 +0100

    xampp fix

--- pages/elements/navbar.php
+++ pages/elements/navbar.php
@@ -1,12 +1,12 @@
 <nav id="navigacio">
-    <img src="/Webterv-projekt-2024/src/img/logo.png" alt="logo" id="logo">
+    <img src="/src/img/logo.png" alt="logo" id="logo">
     <ul>
-        <li><a <?php if ($_SERVER['PHP_SELF'] == "/Webterv-projekt-2024/index.php") echo 'class="active"' ?> href="/Webterv-projekt-2024/index.php">Kezdőlap</a></li>
-        <li><a <?php if ($_SERVER['PHP_SELF'] == "/Webterv-projekt-2024/pages/products.php") echo 'class="active"' ?> href="/Webterv-projekt-2024/pages/products.php">Termékek</a></li>
-        <li><a <?php if ($_SERVER['PHP_SELF'] == "/Webterv-projekt-2024/pages/cart.php") echo 'class="active"' ?> href="/Webterv-projekt-2024/pages/cart.php">Kosár</a></li>
-        <li><a <?php if ($_SERVER['PHP_SELF'] == "/Webterv-projekt-2024/pages/profile.php") echo 'class="active"' ?> href="/Webterv-projekt-2024/pages/profile.php">Profil</a></li>
-        <li><a <?php if ($_SERVER['PHP_SELF'] == "/Webterv-projekt-2024/pages/admin.php") echo 'class="active"' ?> href="/Webterv-projekt-2024/pages/admin.php">Admin</a></li>
-        <li><a <?php if ($_SERVER['PHP_SELF'] == "/Webterv-projekt-2024/pages/login.php") echo 'class="active"' ?> href="/Webterv-projekt-2024/pages/login.php">Bejelentkezés</a></li>
-        <li><a <?php if ($_SERVER['PHP_SELF'] == "/Webterv-projekt-2024/pages/register.php") echo 'class="active"' ?> href="/Webterv-projekt-2024/pages/register.php">Regisztráció</a></li>
+        <li><a <?php if ($_SERVER['PHP_SELF'] == "index.php") echo 'class="active"' ?> href="/index.php">Kezdőlap</a></li>
+        <li><a <?php if ($_SERVER['PHP_SELF'] == "/pages/products.php") echo 'class="active"' ?> href="/pages/products.php">Termékek</a></li>
+        <li><a <?php if ($_SERVER['PHP_SELF'] == "/pages/cart.php") echo 'class="active"' ?> href="/pages/cart.php">Kosár</a></li>
+        <li><a <?php if ($_SERVER['PHP_SELF'] == "/pages/profile.php") echo 'class="active"' ?> href="/pages/profile.php">Profil</a></li>
+        <li><a <?php if ($_SERVER['PHP_SELF'] == "/pages/admin.php") echo 'class="active"' ?> href="/pages/admin.php">Admin</a></li>
+        <li><a <?php if ($_SERVER['PHP_SELF'] == "/pages/login.php") echo 'class="active"' ?> href="/pages/login.php">Bejelentkezés</a></li>
+        <li><a <?php if ($_SERVER['PHP_SELF'] == "/pages/register.php") echo 'class="active"' ?> href="/pages/register.php">Regisztráció</a></li>
     </ul>
 </nav>
