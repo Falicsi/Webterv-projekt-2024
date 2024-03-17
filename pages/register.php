8b49d77c65fdf117d3c36a4f9d50a7bd6310ec64
Béres Ákos Iván <h043580@stud.u-szeged.hu>
2024-03-18 00:11:07 +0100

    register page + css

--- pages/register.php
+++ pages/register.php
@@ -1,3 +1,4 @@
+<?php session_start() ?>
 <!doctype html>
 <html lang="hu">
 <head>
@@ -10,6 +11,29 @@
 </head>
 <body>
     <?php include 'elements/navbar.php'; ?>
-
+    <div class="register content">
+        <div class="register-container">
+            <form action="../src/actions/Register.php" method="POST" id="register-form">
+                <h2>Regisztráció</h2>
+                <div class="input-mezo">
+                    <label for="username">Felhasználónév</label>
+                    <input type="text" placeholder="John Doe" id="username" name="username" required>
+                </div>
+                <div class="input-mezo">
+                    <label for="email">Email</label>
+                    <input type="email" placeholder="johndoe@gmail.com" id="email" name="email" required>
+                </div>
+                <div class="input-mezo">
+                    <label for="password">Jelszó</label>
+                    <input type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;" id="password" name="password" required>
+                </div>
+                <div class="input-mezo">
+                    <label for="password2">Jelszó újra</label>
+                    <input type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;" id="password2" name="password2" required>
+                </div>
+                <input type="submit" value="Regisztráció">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
