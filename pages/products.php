ab0618671ef3d8e4035493b7bc905d07d4c1923f
peszabolcs <perjesidev@gmail.com>
2024-03-23 00:53:50 +0100

    Add products template

--- pages/products.php
+++ pages/products.php
@@ -10,6 +10,108 @@
 </head>
 <body>
     <?php include 'elements/navbar.php'; ?>
+    <div class="products content">
+        <div class="products-container">
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 1</p>
+                <p class="price">6500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 2</p>
+                <p class="price">7500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 3</p>
+                <p class="price">8500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 4</p>
+                <p class="price">9500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 5</p>
+                <p class="price">10500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 6</p>
+                <p class="price">6500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 7</p>
+                <p class="price">7500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 8</p>
+                <p class="price">8500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 9</p>
+                <p class="price">9500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 10</p>
+                <p class="price">10500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 11</p>
+                <p class="price">9500 Ft</p>
+                <div class="tocart">
+                    <button>Kosárba</button>
+                </div>
+            </div>
+            <div class="product-card">
+                <img src="/src/img/product.png" alt="termek">
+                <p class="name">Termék 12</p>
+                <p class="price">10500 Ft</p>
+                <div class="tocart">
+                        <button>Kosárba</button>
+                </div>
+            </div>
+        </div>
+        </div>
+        
+    </div>        
 
 </body>
 </html>
